fa71c09857faf2fc1a4de4292d73e70600d8d85b
Thomas Buitelaar <99041950@mydavinci.nl>
2017-03-22 11:45:57 +0100

    Update index

--- index.php
+++ index.php
@@ -17,9 +17,14 @@
 </nav>
 
 	<form action="" method="" name="">
-		<input type="" name="">
-		<input type="" name="">
-		<input type="" name="">
+		<lable>Welk dier zou je nooit als huisdier willen hebben?</lable><input type="text" name="">
+		<label>Wie is de belangrijkste persoob in je leven?</label><input type="text" name="">
+		<label>In welk land zou je graag willen wonen?</label><input type="text" name="">
+		<label>Wat doe je als je je verveelt?</label><input type="text" name="">
+		<label>Met welk speelgoed speelde je als kind het meest?</label><input type="text" name="">
+		<label>Bij welke docent spijbel je het liefst?</label><input type="text" name="">
+		<label>Als je € 100.00,- had, wat zou je dan kopen?</label><input type="text" name="">
+		<label>Wat is je fovoriete bezigheid?</label><input type="text" name="">
 	</form>
 
 </body>
