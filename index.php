56ea6b7772fb9d957be6f0234525770553814028
Thomas Buitelaar <99041950@mydavinci.nl>
2017-03-24 11:29:49 +0100

    Update index, css Added Index_get.php

--- index.php
+++ index.php
@@ -11,20 +11,20 @@
 <body>
 <h1 id="title">Madlibs</h1>
 <nav>
-<li></li>
-<li></li>
-<li></li>
+<li><a href="index.php">Er heerst paniek...</a></li>
+<li><a href="onkunde.php">Onkunde</a></li>
 </nav>
-
-	<form action="" method="" name="">
-		<lable>Welk dier zou je nooit als huisdier willen hebben?</lable><input type="text" name="">
-		<label>Wie is de belangrijkste persoob in je leven?</label><input type="text" name="">
-		<label>In welk land zou je graag willen wonen?</label><input type="text" name="">
-		<label>Wat doe je als je je verveelt?</label><input type="text" name="">
-		<label>Met welk speelgoed speelde je als kind het meest?</label><input type="text" name="">
-		<label>Bij welke docent spijbel je het liefst?</label><input type="text" name="">
-		<label>Als je € 100.00,- had, wat zou je dan kopen?</label><input type="text" name="">
-		<label>Wat is je fovoriete bezigheid?</label><input type="text" name="">
+	
+	<form action="index_get.php" method="get" name="madlibs">
+		<lable>Welk dier zou je nooit als huisdier willen hebben?</lable><input type="text" name="vr1"><br>
+		<label>Wie is de belangrijkste persoob in je leven?</label><input type="text" name="vr2"><br>
+		<label>In welk land zou je graag willen wonen?</label><input type="text" name="vr3"><br>
+		<label>Wat doe je als je je verveelt?</label><input type="text" name="vr4"><br>
+		<label>Met welk speelgoed speelde je als kind het meest?</label><input type="text" name="vr5"><br>
+		<label>Bij welke docent spijbel je het liefst?</label><input type="text" name="vr6"><br>
+		<label>Als je € 100.00,- had, wat zou je dan kopen?</label><input type="text" name="vr7"><br>
+		<label>Wat is je fovoriete bezigheid?</label><input type="text" name="vr9"><br>
+			<input type="submit" name="submit">
 	</form>
 
 </body>
---
+++ index_get.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Madlibs</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1 id="title">Madlibs</h1>
+<nav>
+<li><a href="index.php">Er heerst paniek...</a></li>
+<li><a href="onkunde.php">Onkunde</a></li>
+</nav>
+
+<h2>Er heerst paniek...</h2>
+<br>
+<p>Er heerst paniek in het koninkrijk Gallifrey. Koning egmond is ten einde raad en als koning<br>
+gmond ten einde raad is, dan roept gij zijn ten-einde-raadsheer <?php echo $_GET["vr1"]; ?>.</p>
+<br>
+<p>"<?php echo $_GET["vr1"]; ?>! HEt is een ramp! Het is een schande!"</p>
+</p>
